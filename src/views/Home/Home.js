17931c1e157f2d2df03197ba281f825fb78ba651
saurabhjaykar1603 <jaykarsaurabh97@gmail.com>
2023-09-29 23:51:29 +0530

    ref : docs into kitchen info

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -4,35 +4,40 @@ import Config from "./../../utils/config.json";
 function Home() {
   return (
     <>
-      <div className="container  p-5" style={{ backgroundColor: Config.theme.accentColor }}>
-        <h1
-          className="text-center mt-3 "
-          style={{ color: Config.theme.primaryColor }}
+      <div className="container px-5">
+        <div
+          className="container  p-5 "
+          style={{ backgroundColor: Config.theme.accentColor }}
         >
-          {" "}
-          {Config.title}
-        </h1>
-        <p
-          className="text-center mt-4 fs-5 fw-bold"
-          style={{ color: Config.theme.secondaryColor }}
-        >
-          {Config.description}
-        </p>
+          <h1
+            className="text-center mt-3 "
+            style={{ color: Config.theme.primaryColor }}
+          >
+            {" "}
+            {Config.title}
+          </h1>
+          <p
+            className="text-center mt-4 fs-5 fw-bold"
+            style={{ color: Config.theme.secondaryColor }}
+          >
+            {Config.description}
+          </p>
 
-        <h2 style={{ color: Config.theme.secondaryColor }}>Services</h2>
-        {Config.services.map((seviceData, i) => {
-          const { title, description, price } = seviceData;
+          <h2 style={{ color: Config.theme.secondaryColor }}>Services</h2>
+          {Config.services.map((seviceData, i) => {
+            const { title, description, price } = seviceData;
 
-          return (
-            <div className="card m-3" key={i}>
-              <div className="card-body">
-                <h3 className="card-title">{title}</h3>
-                <p className="card-text">{description}</p>
-                <p className="card-text">Price: {price}</p>
+            return (
+              <div className="card m-3" key={i}>
+                <div className="card-body">
+                  <h3 className="card-title">{title}</h3>
+                  <p className="card-text">{description}</p>
+                  <p className="card-text">Price: {price}</p>
+                </div>
               </div>
-            </div>
-          );
-        })}
+            );
+          })}
+        </div>
       </div>
     </>
   );
